0e8d882e452f63bfb768448bb81396da1f51727f
BramVanroy <Bram.Vanroy@UGent.be>
2023-04-19 17:57:22 +0200

    add wn preprocessing command

--- src/text2gloss/process_dictionary/vgt_preprocessing.py
+++ src/text2gloss/process_dictionary/vgt_preprocessing.py
@@ -26,13 +26,13 @@ def add_en_translations(df: DataFrame):
 
     @lru_cache(maxsize=256)
     def translate_word(nl_word: str):
+        nl_word = nl_word.strip()
         en_translations = set()
 
         if nl_word[0].isupper():
             # Probably a city, country or name
             en_translations.add(nl_word)
         else:
-            nl_word = nl_word.strip()
             nl_wn_words = nwn.words(nl_word)
 
             if nl_wn_words:
@@ -80,55 +80,96 @@ def remove_brackets(word: str):
     return word
 
 
-def filter_en_translations(df: DataFrame, threshold: float = 0.1):
+def filter_en_translations(df: DataFrame, threshold: float = 0.1, port: int = 5000):
     """Uses fastText embeddings and calculates the centroid of the words in the verified "possible translations" (Dutch) column,
     and for each suggested WordNet translation (English) we calculate the cosine similarity to this centroid, so output value
     is between -1 and +1, NOT between 0 and 1!
 
     :param df: dataframe with "en" (WordNet translations) and "nl" columns ("verified" translations)
     :param threshold: minimal value. If cosine similarity is below this, the word will not be included
+    :param port: local port that the inference server is running on
     :return: the updated DataFrame where potentially items have been removed from the "en" column
     """
     session = requests.Session()
     session.headers.update({"Content-Type": "application/json"})
+    sims = defaultdict(list)  # E.g. {"potato": [("PATAT", 0.1), ("AARDAPPEL", 0.16)]}
+    gloss_variants = defaultdict(list)  # E.g. {"AANRAKEN": ["AANRAKEN-A", "AANRAKEN-B"]}
 
-    def filter_translations(row):
+    def find_similarities(row: Series) -> None:
+        """On the one hand, discard English translations that do not meet the threshold similarity. On the other,
+        collect similarities and gloss variants.
+        - for each gloss, collect its standardized versions in `gloss_variants`
+        - for each English word that meets the thershold, keep track of its similarity score and the std gloss
+        for which this similarity was achieved
+        """
         if not row["nl"] or not row["en"]:
-            return row["en"]
+            return
+
+        std_gloss = standardize_gloss(row["gloss"])
+        gloss_variants[std_gloss].append(row["gloss"])
+
         # Do this for the list, of, words because sometimes
         # there is a comma in the parentheses, which leads to unexpected results such as
         # `bewerkingsteken (+, -...)` -> -...)
         nl_words = remove_brackets(row["nl"])
         nl_words = tuple([w.strip() for w in nl_words.split(", ")])
-        nl_centroid = get_centroid(nl_words, lang="Dutch", session=session)
+        en_words = [w.strip() for w in row["en"].split(", ")]
+
+        # For names/cities that start with a capital letter and that have an equivalent translation in English
+        if len(nl_words) == 1:
+            nl_word = nl_words[0]
+            if nl_word[0].isupper():
+                found = False
+                for en in en_words:
+                    if en[0].isupper():
+                        sims[en].append((std_gloss, 1.0))
+                        found = True
+                        continue
+                # No need to look for other translations
+                if found:
+                    return
+
+        nl_centroid = get_centroid(nl_words, lang="Dutch", session=session, port=port)
 
         if nl_centroid is None:
-            return row["en"]
+            return
 
-        en_words = [w.strip() for w in row["en"].split(", ")]
-        filtered_en = []
         for en in en_words:
-            if not get_token_exists_in_ft(en, lang="English", session=session):
+            en = re.sub(r"^to\s+", "", en)  # Remove "to" in infinitives
+            if not get_token_exists_in_ft(en, lang="English", session=session, port=port):
                 # Do not include it if it is not found in the ft vectors!
+                print("NOPE", en)
                 continue
-            en_vec = get_vec_from_api(en, lang="English", session=session)
-
+            en_vec = get_vec_from_api(en, lang="English", session=session, port=port)
             sim = cos_sim(en_vec, nl_centroid)
-
             if sim >= threshold:
-                filtered_en.append(en)
+                sims[en].append((std_gloss, sim))
             else:
                 logging.info(f"Dropping {en}. Too distant from {nl_words}! (sim={sim:.2f}; threshold={threshold})")
 
-        return ", ".join(filtered_en)
-
-    tqdm.pandas(desc="Filtering EN translations by semantic comparison")
-    df["en"] = df.progress_apply(filter_translations, axis=1)
+    tqdm.pandas(desc="Collecting semantic similarities")
+    df.progress_apply(find_similarities, axis=1)
     session.close()
+
+    df["en"] = ""  # Empty 'en' column so that we can fill it from scratch
+
+    # For every English translation that already reaches the minimal similarity threshold, find for which standardized
+    # gloss (e.g. AANRAKEN) it scored best and then only use the English translation for those glosses
+    # (e.g. AANRAKEN-A, AANRAKEN-B)
+    for en_word, similarities in tqdm(sims.items(), desc="Disambiguating"):
+        sorted_sims = sorted(similarities, key=lambda tup: tup[1], reverse=True)
+        best_sim_std_glos = sorted_sims[0][0]  # row_id with highest similarity
+        # Add this en_word to the potentially existing cell for this ID
+        for gloss in gloss_variants[best_sim_std_glos]:
+            existing_cell = df.loc[df["gloss"] == gloss, "en"].item().split(", ")
+            # Filter empty values
+            existing_cell = [item_strip for item in existing_cell if (item_strip := item.strip())]
+            df.loc[df["gloss"] == gloss, "en"] = ", ".join(sorted(existing_cell+[en_word]))
+
     return df
 
 
-def main(fin: str, threshold: float = 0.1, no_fasttext: bool = False) -> Path:
+def process_vgt_dictionary(fin: str, threshold: float = 0.1, no_fasttext: bool = False, port: int = 5000) -> Path:
     """Generate WordNet translations for the words in the "possible translation" column in the VGT dictionary.
     Then, filter those possible translations by comparing them with the centroid fastText vector. English translations
     that have a cosine similarity (between -1 and +1) of less than the threshold will not be included in the final
@@ -156,7 +197,7 @@ def main(fin: str, threshold: float = 0.1, no_fasttext: bool = False) -> Path:
 
     # Filter/disambiguate translations
     if not no_fasttext:
-        df = filter_en_translations(df, threshold=threshold)
+        df = filter_en_translations(df, threshold=threshold, port=port)
 
     if not had_en_column:
         cols = list(df.columns)
@@ -203,11 +244,20 @@ def main(fin: str, threshold: float = 0.1, no_fasttext: bool = False) -> Path:
     return pfout
 
 
-if __name__ == "__main__":
+def main():
     import argparse
 
-    # TODO: update documentation
-    cparser = argparse.ArgumentParser(description="")
+    cparser = argparse.ArgumentParser(
+        description="'Translates' the VGT dictionary via multilingual WordNet by finding Dutch synsets in Open"
+        " Multilingual WordNet, their corresponding English synset, and all the words belonging to that"
+        " synset. This ccan yield translations that are out-of-context. Therefore we also disambiguate by"
+        " means of aligned fastText vectors. By comparing the vector of every English candidate"
+        " translation with the centroid of the Dutch words' vectors, we can filter out words whose"
+        " similarity to the Dutch words is too low.\nThe script will produce a modified TSV file with an"
+        " 'en' column, as well as a JSON file with gloss-to-english translations (gloss2en), "
+        "gloss-to-dutch translations (gloss2nl), and the respective inverse (en2gloss and nl2gloss).",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
     cparser.add_argument("fin", help="VGT dictionary in TSV format")
     cparser.add_argument(
@@ -220,5 +270,15 @@ if __name__ == "__main__":
     cparser.add_argument(
         "--no_fasttext", action="store_true", help="whether to disable the ambiguity filtering with fastText"
     )
+    cparser.add_argument(
+        "--port",
+        type=int,
+        default=5000,
+        help="Local port that the inference server is running on. Not used when" " '--no_fasttext' is in effect.",
+    )
 
-    main(**vars(cparser.parse_args()))
+    process_vgt_dictionary(**vars(cparser.parse_args()))
+
+
+if __name__ == "__main__":
+    main()
