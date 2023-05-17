bc4db098de88ce4a7404cf018942a90d69b9ad30
Bram Vanroy <Bram.Vanroy@UGent.be>
2023-05-17 18:47:37 +0200

    add option to translate files to glosses

--- src/text2gloss/rule_based/translate.py
+++ src/text2gloss/rule_based/translate.py
@@ -1,23 +1,46 @@
+from pathlib import Path
+
 from text2gloss.utils import send_request
 
 
-def generate_glosses(text, port: int = 5000):
-    output = send_request("rb_text2gloss", port=port, params={"text": text})
-    print(output)
+def generate_glosses(text: str, port: int = 5000):
+    if Path(text).exists():
+        pfin = Path(text)
+        pfout = pfin.with_stem(f"{pfin.stem}-gloss")
+        print(f"Input is a file. Output will be written to {pfout}")
+        sentences = pfin.read_text(encoding="utf-8").splitlines()
+        # Maud's code sometimes adds "+++" and "sweep", but I am not sure what these are. They seem like
+        # sign instructions or pluralization. We do not include those here
+        # Also only include upper-case tokens. Sometimes Maud's code fails, e.g. `eentje` -> `ééntj`
+        sent_glosses = [
+            " ".join(
+                [
+                    gloss.replace("wg+++", "").replace("sweep", "").strip()
+                    for gloss in send_request("rb_text2gloss", port=port, params={"text": text})["glosses"]
+                    if gloss.isupper()
+                ]
+            )
+            for text in sentences
+        ]
+        pfout.write_text("\n".join(sent_glosses) + "\n", encoding="utf-8")
+    else:
+        output = send_request("rb_text2gloss", port=port, params={"text": text})
+        print(output)
 
 
 def main():
     import argparse
 
     cparser = argparse.ArgumentParser(
-        description="'Translates' a given Dutch 'text' to VGT glosses. Assumes that an inference server is running"
-        " on the given 'port' to retrieve spaCy parses.",
+        description="'Translates' a given Dutch 'text' or file to VGT glosses. Assumes that an inference server is"
+        " running on the given 'port'.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
     cparser.add_argument(
         "text",
-        help="Dutch text to translate to VGT glosses",
+        help="Dutch text to translate to VGT glosses. If this is an existing file, the whole file will be translated"
+        " and the output written to a file that is named the same but whose stem ends in -gloss.",
     )
     cparser.add_argument(
         "--port",
